d1b151b67a690acd4ab813d491ce309e632597d1
hariom6474 <hariom6474@gmail.com>
2023-11-09 00:54:28 +0530

    second commit

--- demo.js
+++ demo.js
@@ -1,1 +1,21 @@
 console.log("I love Git");
+ 
+let newName=document.querySelector('title');
+newName.innerHTML = 'Item Lister 2';
+
+let mainHeader=document.querySelector('#main-header');
+mainHeader.innerText = 'Lister';
+console.log(mainHeader.textContent);
+mainHeader.innerHTML = '<h3>Hello</h3>';
+mainHeader.style.borderBottom = 'solid 6px black';
+
+let addItems = document.getElementsByClassName('list-group-item');
+addItems[0].style.color = "green";
+addItems[1].style.color = "green";
+addItems[2].style.color = "green";
+addItems[3].style.color = "green";
+addItems[0].style.fontWeight = "bold";
+addItems[1].style.fontWeight = "bold";
+addItems[2].style.fontWeight = "bold";
+addItems[3].style.fontWeight = "bold";
+
