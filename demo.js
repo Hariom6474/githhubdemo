82c54144dde823ef252c68c6953052ae102c6943
hariom6474 <hariom6474@gmail.com>
2023-11-09 23:25:58 +0530

    getElementsByClassName

--- demo.js
+++ demo.js
@@ -10,12 +10,9 @@ mainHeader.innerHTML = '<h3>Hello</h3>';
 mainHeader.style.borderBottom = 'solid 6px black';
 
 let addItems = document.getElementsByClassName('list-group-item');
-addItems[0].style.color = "green";
-addItems[1].style.color = "green";
-addItems[2].style.color = "green";
-addItems[3].style.color = "green";
+
 addItems[0].style.fontWeight = "bold";
 addItems[1].style.fontWeight = "bold";
 addItems[2].style.fontWeight = "bold";
 addItems[3].style.fontWeight = "bold";
-
+addItems[2].style.backgroundColor = "green";
