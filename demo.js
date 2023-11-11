f1dec82d34c73730e9e3cfa4288f5cb78ca7ffcb
hariom6474 <hariom6474@gmail.com>
2023-11-11 15:46:33 +0530

    getElementByTagName

--- demo.js
+++ demo.js
@@ -8,11 +8,22 @@ mainHeader.innerText = 'Lister';
 console.log(mainHeader.textContent);
 mainHeader.innerHTML = '<h3>Hello</h3>';
 mainHeader.style.borderBottom = 'solid 6px black';
+// getElementByClassName
+// let addItems = document.getElementsByClassName('list-group-item');
 
-let addItems = document.getElementsByClassName('list-group-item');
+// addItems[0].style.fontWeight = "bold";
+// addItems[1].style.fontWeight = "bold";
+// addItems[2].style.fontWeight = "bold";
+// addItems[3].style.fontWeight = "bold";
+// addItems[2].style.backgroundColor = "green";
 
-addItems[0].style.fontWeight = "bold";
-addItems[1].style.fontWeight = "bold";
-addItems[2].style.fontWeight = "bold";
-addItems[3].style.fontWeight = "bold";
-addItems[2].style.backgroundColor = "green";
+// getElementByTagName
+let li = document.getElementsByTagName('li');
+
+li[0].style.fontWeight = 'bold';
+li[1].style.fontWeight = 'bold';
+li[3].style.fontWeight = 'bold';
+li[2].style.backgroundColor = 'green';
+for(let i=0; i<li.length; i++) {
+    li[i].style.backgroundColor = 'yellow';
+}
