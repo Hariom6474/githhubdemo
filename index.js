10e524795980c6c4e8f671b410f8e74edb723f09
hariom6474 <hariom6474@gmail.com>
2023-11-20 22:10:18 +0530

    previous

---
+++ index.js
@@ -0,0 +1,13 @@
+function getFormValue(e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+  console.log(date);
+  console.log(time);
+}
