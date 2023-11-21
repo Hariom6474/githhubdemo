6be3ad963d9a1aab025694f53f78b636fa428a48
hariom6474 <hariom6474@gmail.com>
2023-11-21 19:13:02 +0530

    Lets Scale the app to more users

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 function getFormValue(e) {
   e.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const date = document.getElementById("date").value;
-  const time = document.getElementById("time").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  // const date = document.getElementById("date").value;
+  // const time = document.getElementById("time").value;
   let myObj = {
     name: name,
     email: email,
     phone: phone,
-    date: date,
-    time: time,
+    // date: date,
+    // time: time,
   };
-  localStorage.setItem("myObj", JSON.stringify(myObj));
+  let elem = JSON.stringify(myObj);
+  let ulist = document.querySelector(".list-group");
+  let li = document.createElement("li");
+  li.className = "list-group-items";
+  li.appendChild(document.createTextNode(`${name} - ${email} - ${phone}`));
+  ulist.appendChild(li);
+  localStorage.setItem(email, elem);
   // deserialized can be used if we had to change the value
-  let deserialized = JSON.parse(JSON.stringify(myObj));
-  console.log(deserialized);
+  let deserialized = JSON.parse(elem);
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
 }
