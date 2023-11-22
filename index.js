ce83fd7aa80288013a0289121aab2a5bfe18af43
hariom6474 <hariom6474@gmail.com>
2023-11-22 19:35:52 +0530

    Delete data from Local Storage

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function getFormValue(e) {
   e.preventDefault();
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let phone = document.getElementById("phone").value;
+  let name = e.target.name.value;
+  let email = e.target.email.value;
+  let phone = e.target.phone.value;
   // const date = document.getElementById("date").value;
   // const time = document.getElementById("time").value;
   let myObj = {
@@ -15,12 +15,29 @@ function getFormValue(e) {
   let elem = JSON.stringify(myObj);
   let ulist = document.querySelector(".list-group");
   let li = document.createElement("li");
+  let button = document.createElement("input");
+  button.type = "button";
+  button.value = "Delete";
+  button.id = "myButton";
+  li.appendChild(button);
   li.className = "list-group-items";
   li.appendChild(document.createTextNode(`${name} - ${email} - ${phone}`));
-  ulist.appendChild(li);
+  if (ulist) {
+    ulist.appendChild(li);
+  }
+  button.addEventListener("click", function (e) {
+    let li = e.target.closest("li");
+    if (li) {
+      localStorage.removeItem(email, elem);
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("phone").value = "";
+      ulist.removeChild(li);
+    }
+  });
   localStorage.setItem(email, elem);
   // deserialized can be used if we had to change the value
-  let deserialized = JSON.parse(elem);
+  // let deserialized = JSON.parse(elem);
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("phone").value = "";
